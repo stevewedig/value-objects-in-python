d09e86f680ed642043ee51ef8ad937f5b8ce7501
Steve Wedig <stevewedig@gmail.com>
2014-07-26 16:27:59 -0700

    distro

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ from distutils.core import setup
 # http://guide.python-distribute.org/creation.html
 setup( 
     name = 'value_objects',
-    version = '2.0.0',
+    version = '2.0.1',
     author = 'Steve Wedig',
-    packages = ['value_objects'],
+    packages = [
+      'value_objects', 
+      'value_objects.mixins',
+      'value_objects.opt', 
+      'value_objects.util',
+    ],
     url = 'https://github.com/stevewedig/value-objects-in-python/',
     license = 'LICENSE.txt',
     description = 'Helpers for implementing value objects.',
