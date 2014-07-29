9878180d30dc111cbbe15fbd93bdacaa52d13278
Steve Wedig <stevewedig@gmail.com>
2014-07-29 13:55:35 -0700

    isinstance

--- value_objects/mixins/object_helper.py
+++ value_objects/mixins/object_helper.py
@@ -3,7 +3,7 @@ from value_objects.util.compat import izip, unicode
 from value_objects.util.once import once
 
 class ObjectHelper( object ):
-  
+
   def __init__( self, object_class, field_names, field_values ):
     self.object_class = object_class
     self.field_names = field_names
@@ -16,14 +16,14 @@ class ObjectHelper( object ):
   @once
   def class_and_state( self ):
     return ( self.object_class, self.field_values )
-    
+
   # ====================================
   # strs
   # ====================================
 
   @once
   def class_and_state_unicode( self ):
-    
+
     pairStrs = ( 
       '%s=%s' % ( name, unicode( value ) ) for name, value in self.field_pairs
     )
@@ -31,10 +31,10 @@ class ObjectHelper( object ):
     body = ', '.join( pairStrs )
 
     return '%s{%s}' % ( self.object_class.__name__, body )
-    
+
   @once
   def class_and_state_repr( self ):
-    
+
     pairStrs = ( 
       '%s=%s' % ( name, repr( value ) ) for name, value in self.field_pairs
     )
@@ -52,9 +52,12 @@ class ObjectHelper( object ):
       other_helper = other_object.object_helper
     except AttributeError:
       return False
-  
+
+    if not isinstance( other_helper, ObjectHelper ):
+      return False
+
     return self.class_and_state == other_helper.class_and_state
-  
+
   # ====================================
   # hash
   # ====================================
