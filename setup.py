f73016994d1ac7af0917bf48522583357f1d5edc
Steve Wedig <stevewedig@gmail.com>
2014-07-29 16:07:22 -0700

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 # update: python setup.py sdist upload
 setup( 
     name = 'value_objects',
-    version = '2.0.2',
+    version = '2.0.3',
     author = 'Steve Wedig',
     packages = [
       'value_objects', 
